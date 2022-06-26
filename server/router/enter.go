5b274053d4ab5dcdfe33f06b0a342f73e8b860dc
siuvlqnm <siuvlqnm@gmail.com>
2022-06-26 15:53:40 +0800

    add package user card

--- server/router/enter.go
+++ server/router/enter.go
@@ -1,13 +1,17 @@
 package router
 
 import (
+	"github.com/flipped-aurora/gin-vue-admin/server/router/card"
 	"github.com/flipped-aurora/gin-vue-admin/server/router/example"
 	"github.com/flipped-aurora/gin-vue-admin/server/router/system"
+	"github.com/flipped-aurora/gin-vue-admin/server/router/user"
 )
 
 type RouterGroup struct {
-	System   system.RouterGroup
-	Example  example.RouterGroup
+	System  system.RouterGroup
+	Example example.RouterGroup
+	User    user.RouterGroup
+	Card    card.RouterGroup
 }
 
 var RouterGroupApp = new(RouterGroup)
