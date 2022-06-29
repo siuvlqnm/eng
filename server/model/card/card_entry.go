92a44dca18468c64cfd91664b973b303ec8bd0eb
siuvlqnm <siuvlqnm@gmail.com>
2022-06-29 17:13:41 +0800

    add card

--- server/model/card/card_entry.go
+++ server/model/card/card_entry.go
@@ -11,10 +11,9 @@ import (
 // 如果含有time.Time 请自行import time包
 type CardEntry struct {
 	global.GVA_MODEL
-	CardName   string `json:"cardName" form:"cardName" gorm:"column:card_name;not null;comment:入场卡名称;"`
-	CardType   uint8  `json:"cardType" form:"cardType" gorm:"column:card_type;not null;default:1;comment:入场卡类型：1次卡，2期限卡;"`
-	SuptUserNr uint8  `json:"suptUserNr" form:"suptUserNr" gorm:"column:support_user_number;not null;default:1;comment:支持入场人数;"`
-
+	CardName      string    `json:"cardName" form:"cardName" gorm:"column:card_name;not null;comment:入场卡名称;"`
+	CardType      uint8     `json:"cardType" form:"cardType" gorm:"column:card_type;not null;default:1;comment:入场卡类型：1次卡，2期限卡;"`
+	SuptUserNr    uint8     `json:"suptUserNr" form:"suptUserNr" gorm:"column:support_user_number;not null;default:1;comment:支持入场人数;"`
 	CardStat      uint8     `json:"cardStat" form:"cardStat" gorm:"column:card_status;not null;default:1;comment:上架状态：1已上架可售，2已下架可售，3已下架停售;"`
 	SaleType      uint8     `json:"saleType" form:"saleType" gorm:"column:sale_type;not null;default:1;comment:售卖类型：1永久售卖，2限时售卖;"`
 	SaleStartTime time.Time `json:"saleStartTime" form:"saleStartTime" gorm:"column:sale_time_time;type:timestamp;not null;comment:售卖开始时间;"`
--- server/model/card/card_entry_specs.go
+++ server/model/card/card_entry_specs.go
@@ -4,6 +4,11 @@ import "github.com/flipped-aurora/gin-vue-admin/server/global"
 
 type CardEntrySpecs struct {
 	global.GVA_MODEL
-	CardEntryId uint `gorm:"column:card_entry_id;not null;comment:入场卡ID;"`
-	Price       uint `gorm:"column:price;not null;comment:价格;"`
+	CardEntryId uint   `gorm:"column:card_entry_id;not null;comment:入场卡ID;"`
+	Price       uint   `gorm:"column:price;not null;default:0;comment:价格;"`
+	ValidTime   uint   `gorm:"column:valid_time;not null;default:0;comment:有效次数;"`
+	ValidPeriod uint   `gorm:"column:valid_period;not null;default;1;comment:有效周期;"`
+	DateType    string `gorm:"column:date_type;not null;default:1;comment:日期类型：1天，2月，3年;"`
+	FreezeAllow uint   `gorm:"column:freeze_allow;not null;default:0;comment:允许冻结天数;"`
+	GiftAllow   uint   `gorm:"column:gift_allow;not null;default:0;comment:允许赠送天数;"`
 }
